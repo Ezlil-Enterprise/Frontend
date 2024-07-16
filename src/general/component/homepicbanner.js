cfc466e674472bf330d4e71fc9fa527a1496c04d
P3F0004I-ShanjaiKumaar <sn.shanjaikumaar@p3fusion.com>
2024-07-16 20:15:26 +0530

    updating   landing page

--- src/general/component/homepicbanner.js
+++ src/general/component/homepicbanner.js
@@ -1,32 +1,77 @@
 import React from "react";
-import { Col, Row, Space, Typography } from "antd";
-import Land from "../asset/image/landing.jpg";
+import { Button, Carousel, Col, Row, Image, Typography } from "antd";
+import Demo from "../asset/image/demo_product.png";
 import "../asset/less/typography-ls.less";
 import "../asset/css/global.css";
+import { MB10 } from "./widget";
+
 const Homepicbanner = () => {
   return (
-    <Row style={{ height: "90vh" }}>
+    <Row style={{ padding: "48px", backgroundColor: "#fff" }}>
       <Col
         span={24}
-        className="home-landing-banner"
-        style={{
-          backgroundImage: `url(${Land})`,
-        
-        }}
+        style={{ backgroundColor: "#D5DCD7", borderRadius: "24px" }}
       >
-        <Space direction='vertical'>
-          {" "}
-          <Typography className="ez-ls-h1 secondary">
-            Ezlil Solutions
-          </Typography>
-          <Typography className="ez-ls-h3 brand-green">
-            Experience the difference organic makes. Crafted with pure,
-            plant-based ingredients, our soaps gently cleanse and nourish,
-            leaving your skin feeling soft and radiant. Shop now and discover a
-            healthier shower routine.
-          </Typography>
-          <Typography className="ez-ls-h5 brand-green underline">Join Now</Typography>
-        </Space>
+        <Carousel effect="fade">
+          <div>
+            <Row style={{ padding: "48px" }}>
+              <Col span={24}>
+                <Typography className="ez-ls-h4 gray_v2">Skincare</Typography>
+              </Col>
+              <Col span={24}>
+                <Typography className="ez-ls-h1-s1 black">
+                  Body Lotion
+                </Typography>
+              </Col>
+              <Col
+                span={24}
+                style={{
+                  backgroundImage: `url(${Demo})`,
+                  backgroundRepeat: "no-repeat",
+                  height: "100%",
+                  backgroundPosition: "center",
+                }}
+              >
+                <Typography className="ez-ls-hs1" style={{ zIndex: "-1" }}>
+                  Nature Care
+                </Typography>
+              </Col>
+              <MB10 />
+              <Col span={24} align="start">
+                <Button className="colored-background bg-btn" shape="round">
+                  Buy Now
+                </Button>
+              </Col>
+              <MB10 />
+              <Col
+                span={24}
+                align="end"
+                style={{ display: "flex", justifyContent: "end" }}
+              >
+                <div style={{ width: "30%" }}>
+                  <Typography className="ez-ls-h4 bold">Description</Typography>
+                  <Typography className="ez-ls-h5 gray_v2">
+                    Using the power of nature to produce the most beautiful glow
+                    to your Skin.
+                  </Typography>
+                  <Typography className="ez-ls-h4 bold primary">
+                    Read More
+                  </Typography>
+                </div>
+              </Col>
+              <MB10 />
+            </Row>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+        </Carousel>
       </Col>
     </Row>
   );
